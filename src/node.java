0ac5af8830be875b48d7b37615e065968d3798eb
Marco Blum <marco8jnv@hotmail.com>
2018-03-20 23:15:20 +0100

    small fix

--- src/node.java
+++ src/node.java
@@ -2,7 +2,7 @@ import java.util.ArrayList;
 
 public class node {
     private node yes,no;
-    private ArrayList<String> data = new ArrayList();
+    private ArrayList<String> data;
     private ArrayList<Boolean> combo = new ArrayList();
     private String msg;
     public node(){}
@@ -10,7 +10,7 @@ public class node {
         this.combo = combo;
         this.combo.add(me);
         if(this.combo.size() == 4 ){
-            if(this.combo.get(0) == false || (this.combo.get(1) == false && this.combo.get(2) == false) || this.combo.get(1) == false && this.combo.get(3) == false){
+            if(this.combo.get(0) == false || (this.combo.get(1) == false && this.combo.get(2) == false) || (this.combo.get(1) == false && this.combo.get(3) == false)){
                 msg = "Warning!";
             }
             else{
@@ -18,9 +18,15 @@ public class node {
             }
         }
     }
-
+    /*
+    points is a list of the criterias in order: hand ins, attend lectures, read textbook & make exercises
+    as this is the most optimal way to do it
+    */
     public void putStudent(ArrayList<Boolean> points, String student){
         if(points.size() == 0){
+            if(data == null){
+                data = new ArrayList();
+            }
             data.add(student);
         }
         else{
@@ -37,10 +43,13 @@ public class node {
         }
     }
     public void print(int tree){
-        if(tree == 0){
+        if(tree == 0 || data != null){
+            if(data == null){
+                data = new ArrayList();
+            }
             System.out.println(msg+"  :  "+combo+" "+data.toString());
         }
-        else {
+        if(yes != null){
             yes.print(tree-1);
             no.print(tree-1);
         }
